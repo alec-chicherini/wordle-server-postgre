fa202edb4a77503d052951a41bfe96ccb983e4b3
Alexey Chicherin <chicherin.alexey@ya.ru>
2025-03-19 18:29:53 +0300

    Добавлена таблица game_row_response

--- sqls/create_server_game_tables.sql
+++ sqls/create_server_game_tables.sql
@@ -9,3 +9,27 @@ CREATE TABLE IF NOT EXISTS server_game_schema.game_sessions(
 	user_uuid UUID,
 	word_answer TEXT
 );
+-- https://github.com/alec-chicherini/wordle-data/blob/main/proto/enum_row_result.proto
+CREATE TYPE server_game_schema.enum_row_result AS ENUM (
+	'kNoneRowResult',
+	'kWordDoNotExists',
+	'kWordExists',
+	'kWordIsAnswer',
+	'kServerError',
+	'kGameOver'
+);
+
+-- https://github.com/alec-chicherini/wordle-data/blob/main/proto/enum_the_char_color.proto
+CREATE TYPE server_game_schema.enum_the_char_color AS ENUM (
+	'kNoneTheCharColor',
+	'kGreen',
+	'kYellow'
+);
+
+CREATE TABLE IF NOT EXISTS server_game_schema.game_row_response(
+	response_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid() ,
+	game_uuid UUID ,
+	word_asked TEXT,
+	result server_game_schema.enum_row_result,
+	vec_colors server_game_schema.enum_the_char_color[5]
+);
